c56fd8dff2879a36286926f087a1cdc9c248a65f
bmatchen <bmatchen@gmail.com>
2022-12-04 16:18:50 -0500

    start fixing clippy findings

--- src/util.rs
+++ src/util.rs
@@ -5,14 +5,13 @@ use std::path::Path;
 pub(crate) fn get_file_contents(filename: &str) -> String {
     let path = Path::new(filename);
 
-    let mut file = match File::open(&path) {
+    let mut file = match File::open(path) {
         Err(why) => panic!("Failed to open file at {}: {}", filename, why),
         Ok(file) => file,
     };
     let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("{}", why),
-        Ok(_) => (),
+    if let Err(why) = file.read_to_string(&mut s) {
+        panic!("{}", why)
     }
     s
 }
