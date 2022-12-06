92ea7b998bdbed3405ba780160e7d90de22b9e81
bmatchen <bmatchen@gmail.com>
2022-12-06 00:41:15 -0500

    solved day 06

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod day_02;
 mod day_03;
 mod day_04;
 mod day_05;
+mod day_06;
 mod util;
 
 use std::time::Instant;
@@ -16,6 +17,7 @@ fn main() {
     day_03::day_03("../data/03.txt");
     day_04::day_04("../data/04.txt");
     day_05::day_05("../data/05.txt");
+    day_06::day_06("../data/06.txt");
     let elapsed = now.elapsed();
     println!("Ran all (implemented) solutions in {:?}", elapsed);
 }
