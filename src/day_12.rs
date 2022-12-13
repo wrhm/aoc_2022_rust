253597e55ce86a0ed04c69d8dd0bdf87e303c457
bmatchen <bmatchen@gmail.com>
2022-12-12 21:23:00 -0500

    solve day 12 part 2

--- src/day_12.rs
+++ src/day_12.rs
@@ -11,6 +11,62 @@ fn can_reach(ch_from: char, ch_to: char) -> bool {
         || (ch_to != 'E' && (ch_to as i32 - ch_from as i32) <= 1)
 }
 
+fn find_shortest_path(
+    from_r: usize,
+    from_c: usize,
+    to_r: usize,
+    to_c: usize,
+    h: usize,
+    w: usize,
+    hm: &HashMap<(usize, usize), char>,
+) -> i32 {
+    let mut seen: HashSet<(usize, usize)> = HashSet::new();
+    let mut q: VecDeque<(usize, usize, i32)> = VecDeque::new();
+    let sr = from_r;
+    let sc = from_c;
+    let er = to_r;
+    let ec = to_c;
+
+    q.push_back((sr as usize, sc as usize, 0));
+    while !q.is_empty() {
+        let (r, c, dist) = q.pop_front().unwrap();
+        if seen.contains(&(r, c)) {
+            continue;
+        }
+        seen.insert((r, c));
+        if r == er && c == ec {
+            return dist;
+        }
+
+        let chcurr = *hm.get(&(r, c)).unwrap();
+        if r >= 1 && hm.contains_key(&(r - 1, c)) {
+            let chnxt = *hm.get(&(r - 1, c)).unwrap();
+            if can_reach(chcurr, chnxt) && !seen.contains(&(r - 1, c)) {
+                q.push_back((r - 1, c, dist + 1));
+            }
+        }
+        if r < h - 1 && hm.contains_key(&(r + 1, c)) {
+            let chnxt = *hm.get(&(r + 1, c)).unwrap();
+            if can_reach(chcurr, chnxt) && !seen.contains(&(r + 1, c)) {
+                q.push_back((r + 1, c, dist + 1));
+            }
+        }
+        if c >= 1 && hm.contains_key(&(r, c - 1)) {
+            let chnxt = *hm.get(&(r, c - 1)).unwrap();
+            if can_reach(chcurr, chnxt) && !seen.contains(&(r, c - 1)) {
+                q.push_back((r, c - 1, dist + 1));
+            }
+        }
+        if c < w - 1 && hm.contains_key(&(r, c + 1)) {
+            let chnxt = *hm.get(&(r, c + 1)).unwrap();
+            if can_reach(chcurr, chnxt) && !seen.contains(&(r, c + 1)) {
+                q.push_back((r, c + 1, dist + 1));
+            }
+        }
+    }
+    -1 // not found
+}
+
 fn day_12_both_parts(file_contents: &str) -> (i32, i32) {
     let lines: Vec<&str> = file_contents.split('\n').collect();
     let h = lines.len();
@@ -33,60 +89,26 @@ fn day_12_both_parts(file_contents: &str) -> (i32, i32) {
         }
     }
 
-    let mut ans1 = -1;
-    let mut seen: HashSet<(usize, usize)> = HashSet::new();
-    let mut q: VecDeque<(usize, usize, i32)> = VecDeque::new();
-    // let mut HashSet<(usize)>
-    let mut parent: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
-    q.push_back((sr as usize, sc as usize, 0));
-    while !q.is_empty() {
-        let (r, c, dist) = q.pop_front().unwrap();
-        if seen.contains(&(r, c)) {
+    let ans1 = find_shortest_path(
+        sr as usize,
+        sc as usize,
+        er as usize,
+        ec as usize,
+        h,
+        w,
+        &hm,
+    );
+
+    let mut ans2 = (w * h + 1) as i32;
+    for ((ar, ac), &letter) in &hm {
+        if letter != 'a' {
             continue;
         }
-        seen.insert((r, c));
-        if r as i32 == er && c as i32 == ec {
-            ans1 = dist;
-            break;
-        }
-
-        let chcurr = *hm.get(&(r, c)).unwrap();
-        if r >= 1 && hm.contains_key(&(r - 1, c)) {
-            let chnxt = *hm.get(&(r - 1, c)).unwrap();
-            if can_reach(chcurr, chnxt) && !seen.contains(&(r - 1, c)) {
-                parent.insert((r - 1, c), (r, c));
-                q.push_back((r - 1, c, dist + 1));
-            }
-        }
-        if r < h - 1 && hm.contains_key(&(r + 1, c)) {
-            let chnxt = *hm.get(&(r + 1, c)).unwrap();
-            if can_reach(chcurr, chnxt) && !seen.contains(&(r + 1, c)) {
-                parent.insert((r + 1, c), (r, c));
-                q.push_back((r + 1, c, dist + 1));
-            }
-        }
-        if c >= 1 && hm.contains_key(&(r, c - 1)) {
-            let chnxt = *hm.get(&(r, c - 1)).unwrap();
-            if can_reach(chcurr, chnxt) && !seen.contains(&(r, c - 1)) {
-                parent.insert((r, c - 1), (r, c));
-                q.push_back((r, c - 1, dist + 1));
-            }
-        }
-        if c < w - 1 && hm.contains_key(&(r, c + 1)) {
-            let chnxt = *hm.get(&(r, c + 1)).unwrap();
-            if can_reach(chcurr, chnxt) && !seen.contains(&(r, c + 1)) {
-                parent.insert((r, c + 1), (r, c));
-                q.push_back((r, c + 1, dist + 1));
-            }
+        let fsp = find_shortest_path(*ar, *ac, er as usize, ec as usize, h, w, &hm);
+        if fsp != -1 {
+            ans2 = std::cmp::min(ans2, fsp);
         }
     }
-    let mut pos = (er as usize, ec as usize);
-    while parent.contains_key(&pos) {
-        let next = *parent.get(&pos).unwrap();
-        pos = next;
-    }
-
-    let ans2 = 0;
     (ans1, ans2)
 }
 
@@ -106,8 +128,8 @@ mod tests {
     #[test]
     fn unit_test() {
         let file_contents = util::get_file_contents("test_data/12.txt");
-        let (ans1, _) = day_12_both_parts(&file_contents);
+        let (ans1, ans2) = day_12_both_parts(&file_contents);
         assert_eq!(ans1, 31);
-        // assert_eq!(ans2, -1);
+        assert_eq!(ans2, 29);
     }
 }
